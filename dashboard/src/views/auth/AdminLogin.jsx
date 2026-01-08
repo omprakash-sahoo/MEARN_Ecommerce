1653800d45b61bf829a9c3182cd1a21581da3850
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2026-01-08 23:54:52 +0530

    implement admin login flow, API auth, and reducer improvements hotstoster for success and failed message

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { admin_login } from "../../store/Reducers/authReducer";
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { admin_login, messageClear } from "../../store/Reducers/authReducer";
+import { PropagateLoader } from "react-spinners";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 const AdminLogin = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { loader, errorMessage, successMessage } = useSelector(
+    (store) => store.auth
+  );
   const [state, setState] = useState({
     email: "",
     password: "",
   });
-
+  const overrideStyle = {
+    display: "flex",
+    margin: "0 auto",
+    height: "24px",
+    justifyContent: "center",
+    alignItem: "center",
+  };
   const inputHandle = (e) => {
     setState({
       ...state,
@@ -20,6 +32,17 @@ const AdminLogin = () => {
     e.preventDefault();
     dispatch(admin_login(state));
   };
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+      navigate("/");
+    }
+  }, [errorMessage, successMessage]);
 
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
@@ -65,7 +88,11 @@ const AdminLogin = () => {
             </div>
 
             <button className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-              Login
+              {loader ? (
+                <PropagateLoader color="#fff" cssOverride={overrideStyle} />
+              ) : (
+                "Login"
+              )}
             </button>
           </form>
         </div>
